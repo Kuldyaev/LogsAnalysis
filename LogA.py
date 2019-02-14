11d751d933473f72f7caeb57be6287f490f3fc8b
Kuldyaev Viacheslav <slavalion1@gmail.com>
2019-02-14 22:15:22 +0300

    Update LogA.py

--- LogA.py
+++ LogA.py
@@ -18,4 +18,21 @@ def work(query):
 
 
 if __name__ == '__main__':
-    work()
+    work("""
+        SELECT total.day,
+          ROUND(((errors.error_requests*1.0) / total.requests), 3) AS percent
+        FROM (
+          SELECT date_trunc('day', time) "day", count(*) AS error_requests
+          FROM log
+          WHERE status LIKE '404%'
+          GROUP BY day
+        ) AS errors
+        JOIN (
+          SELECT date_trunc('day', time) "day", count(*) AS requests
+          FROM log
+          GROUP BY day
+          ) AS total
+        ON total.day = errors.day
+        WHERE (ROUND(((errors.error_requests*1.0) / total.requests), 3) > 0.01)
+        ORDER BY percent DESC;
+    """)
