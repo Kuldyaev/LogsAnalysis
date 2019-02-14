8d3daec575a1705d6c6c28abdb683eea6268cdda
Kuldyaev Viacheslav <slavalion1@gmail.com>
2019-02-14 20:05:13 +0300

    Update LogA.py

--- LogA.py
+++ LogA.py
@@ -3,22 +3,22 @@
 import psycopg2
 
 def main():
-    try: 
-	    db = psycopg2.connect(database='news' , user='postgres' , password='postgres' , port=5432, host='localhost')
-	    print ("OK")
-	    cursor=db.cursor()
-	    cursor.execute('SELECT * FROM log LIMIT 5')
-	    for row in cursor:
-		    print (row)
+    try:
+        db = psycopg2.connect(database='news' , user='postgres' , password='postgres' , port=5432, host='localhost')
+        print ("OK")
+        cursor=db.cursor()
+        cursor.execute('SELECT * FROM log LIMIT 5')
+        for row in cursor:
+            print (row)
         cursor.close()
         cursor.execute()
         results = cursor.fetchall('SELECT * FROM authors LIMIT 2')
-		for article in results:
+        for article in results:
             title = article[0]
             views = article[1]
             print("\"%s\" - %s views." % (title, views))
         cursor.close()
-	    db.close()
+        db.close()
     except:
 	    print ('FAIL')
 
