4c229ac5cc7495bec60819c66961ead0f787ee09
Kuldyaev Viacheslav <slavalion1@gmail.com>
2019-02-15 00:18:34 +0300

    Update LogA.py

--- LogA.py
+++ LogA.py
@@ -16,6 +16,26 @@ def work(query):
 
 dict = {
 1 : ['q1', [ """
+        SELECT articles.title, COUNT(*) AS num
+        FROM articles
+        JOIN log
+        ON log.path LIKE concat('/article/%', articles.slug)
+        GROUP BY articles.title
+        ORDER BY num DESC
+        LIMIT 3;
+    """] , 'ans1'],
+2 : ['q2', ["""
+        SELECT authors.name, COUNT(*) AS num
+        FROM authors
+        JOIN articles
+        ON authors.id = articles.author
+        JOIN log
+        ON log.path like concat('/article/%', articles.slug)
+        GROUP BY authors.name
+        ORDER BY num DESC
+        LIMIT 3;
+    """], 'ans2'],
+3 : ['q3', ["""
         SELECT total.day,
           ROUND(((errors.error_requests*1.0) / total.requests), 3) AS percent
         FROM (
@@ -32,15 +52,14 @@ dict = {
         ON total.day = errors.day
         WHERE (ROUND(((errors.error_requests*1.0) / total.requests), 3) > 0.01)
         ORDER BY percent DESC;
-    """] , 'ans1'],
-2 : ['q2', 'sql2', 'ans2'],
-3 : ['q3', 'sql3', 'ans3']
+    """], 'ans3']
 }        
         
         
 def main():
-    query = dict[1][1][0]    
-    answer = work(query)
-    print(answer)
+    for i in range(4):
+        query = dict[i][1][0]    
+        answer = work(query)
+        print(answer)
 if __name__ == '__main__':
     main()
