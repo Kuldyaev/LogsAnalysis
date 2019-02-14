3e8725bc500cd6b7cfefa2546d4397782b1e4aeb
Kuldyaev Viacheslav <slavalion1@gmail.com>
2019-02-14 11:51:28 -0500

    Add files via upload

---
+++ LogA.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import psycopg2
+
+def main():
+    try: 
+	     db = psycopg2.connect(database='news' , user='postgres' , password='postgres' , port=5432, host='localhost')
+	     print ("OK")
+	     cursor=db.cursor()
+	     cursor.execute('SELECT * FROM log LIMIT 5')
+	     for row in cursor:
+		     print (row)
+	     cursor.close()
+	     cursor.execute()
+         results = cursor.fetchall('SELECT * FROM authors LIMIT 2')
+         for article in results:
+             title = article[0]
+             views = article[1]
+             print("\"%s\" - %s views." % (title, views))
+         cursor.close()
+	     db.close()
+    except:
+	    print ('FAIL')
+
+
+
+
+if __name__ == '__main__':
+     main()
