c99da439f4d06df2932a9baa5849eaafc3e3741d
Kuldyaev Viacheslav <slavalion1@gmail.com>
2019-02-15 04:26:18 +0300

    Update LogA.py

--- LogA.py
+++ LogA.py
@@ -63,11 +63,11 @@ def main():
         answer = work(query)
         dict[i][2] = answer
         statment += '\n Question' + i +': ' + str(dict[1][0]) + '\n Answer' + i + ': '
-        if i=1:
+        if i==1:
             for rec in dict[1][2]:
                stat01 = (rec[0] + " -- " + str(rec[2]) + " views")
             statment +=  stat01
-        elif i=2:
+        elif i==2:
             for rec in dict[1][2]:
                 stat02 =(rec[1] + " -- " + str(rec[0]) + " views")
             statment +=  stat02
