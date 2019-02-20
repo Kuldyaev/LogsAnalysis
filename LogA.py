9d31319ff3a9eb28011cfcaa8c7f1f3d16fea4c9
Kuldyaev Viacheslav <slavalion1@gmail.com>
2019-02-20 22:16:09 +0300

    Update LogA.py

--- LogA.py
+++ LogA.py
@@ -23,19 +23,22 @@ dict = {
              GROUP BY articles.title 
 	     ORDER BY hits DESC 
 	     LIMIT 3; """] , 'ans1'],
-2 : ['Who are the most popular article authors of all time?', ["""SELECT count(l.id) as count, au.name 
-                FROM articles a JOIN log l ON l.path = concat('/article/', a.slug) 
-                JOIN authors au 
-                ON au.id = a.author 
-                GROUP BY au.name
-                ORDER BY count DESC 
-                LIMIT 3; """], 'ans2'],
+2 : ['Who are the most popular article authors of all time?', 
+     ["""SELECT authors.name, COUNT(*) AS num
+        FROM authors
+        JOIN articles
+        ON authors.id = articles.author
+        JOIN log
+        ON log.path LIKE CONCAT('/article/%', articles.slug)
+        GROUP BY authors.name
+        ORDER BY num DESC
+        LIMIT 3;"""], 'ans2'],
 3 : ['On which days did more than 1% of requests lead to errors?', 
-        ["""SELECT *
-        FROM error_rate
-        WHERE error_rate.percentage > 1
-        ORDER BY error_rate.percentage DESC;
-        """], 'ans3']
+        ["""SELECT to_char(time, 'DD FMMonth YYYY') AS date,
+        (count(status) filter (where status like '404%'))
+        * 100.0 / (count(status)) as errors
+        FROM log group BY date
+        having errors (status) > 1.0;"""], 'ans3']
 }        
         
         
