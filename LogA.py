bed78f3665161bc21a41e144943fd1583166e712
Kuldyaev Viacheslav <slavalion1@gmail.com>
2019-02-21 06:52:53 +0300

    Update LogA.py

--- LogA.py
+++ LogA.py
@@ -62,7 +62,7 @@ def main():
                 stat02 =("\n \t" + rec[0] + " -- " + str(rec[1]) + " views")
                 statment +=  stat02
         else:
-            statment += "\n \t" + answer + " -- " + answer
+            statment += "\n \t" + str(answer[0]) + " -- " + str(answer[0])
         dict[i][2] = answer
     print (statment) 
  
