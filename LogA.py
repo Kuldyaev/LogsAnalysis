f0ea3375284953a09fa34d3498322979598d7391
Kuldyaev Viacheslav <slavalion1@gmail.com>
2019-02-23 08:15:45 +0300

    Update LogA.py

--- LogA.py
+++ LogA.py
@@ -53,11 +53,11 @@ def main():
         statment += '\n Question ' + str(i) +': ' + str(dict[i][0]) + '\n Answer ' + str(i) + ': '
         if i==1:
             for rec in answer:
-                stat01 = ("\n \t" + u'\u2022' + '\"' + rec[0] + '\"' + " -- " + str(rec[1]) + " views")
+                stat01 = ("\n \t" + u'\u2022' + ' \"' + rec[0] + '\"' + " -- " + str(rec[1]) + " views")
                 statment +=  stat01
         elif i==2:
             for rec in answer:
-                stat02 =("\n \t" + u'\u2022' + rec[0] + " -- " + str(rec[1]) + " views")
+                stat02 =("\n \t" + u'\u2022'+ ' ' + rec[0] + " -- " + str(rec[1]) + " views")
                 statment +=  stat02
         else:
             statment += "\n \t" + str(answer[0][0]) + "  " + str(answer[0][1]) + '%' + ' of requests lead to errors'
